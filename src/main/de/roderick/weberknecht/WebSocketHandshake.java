1f658a3d52523ddaedb567edfe3312e50da41b5a
Timo Savola <timo@ninchat.com>
2014-06-10 11:58:37 +0300

    case-insensitive "Connection: upgrade" matching

--- src/main/de/roderick/weberknecht/WebSocketHandshake.java
+++ src/main/de/roderick/weberknecht/WebSocketHandshake.java
@@ -115,7 +115,7 @@ public class WebSocketHandshake
 	{
 		if (!headers.get("Upgrade").toLowerCase().equals("websocket")) {
 			throw new WebSocketException("connection failed: missing header field in server handshake: Upgrade");
-		} else if (!headers.get("Connection").equals("Upgrade")) {
+		} else if (!headers.get("Connection").toLowerCase().equals("upgrade")) {
 			throw new WebSocketException("connection failed: missing header field in server handshake: Connection");
 		}
 	}
